c7b3391f16bbd34f78b5b22f864cf42067c1e458
AliRezaTaleghani <shayne.alone@gmail.com>
2026-02-09 20:52:37 +0100

    fix minor issues

--- app/src/main/java/com/github/opscalehub/avacore/TtsDataCheckActivity.kt
+++ app/src/main/java/com/github/opscalehub/avacore/TtsDataCheckActivity.kt
@@ -22,20 +22,26 @@ class TtsDataCheckActivity : Activity() {
 
         when (action) {
             TextToSpeech.Engine.ACTION_CHECK_TTS_DATA -> {
-                // فقط یک کد برمی‌گردانیم تا لیست تکراری نشود
-                // fa-IR استانداردترین حالت برای اندروید است
-                val availableVoices = arrayListOf("fa-IR")
+                // Return supported locales in different formats to ensure compatibility
+                val availableVoices = arrayListOf("fa", "fa-IR", "fas-IRN")
                 resultIntent.putStringArrayListExtra(TextToSpeech.Engine.EXTRA_AVAILABLE_VOICES, availableVoices)
-                resultIntent.putStringArrayListExtra(TextToSpeech.Engine.EXTRA_UNAVAILABLE_VOICES, arrayListOf<String>())
+                resultIntent.putStringArrayListExtra(TextToSpeech.Engine.EXTRA_UNAVAILABLE_VOICES, arrayListOf())
+                
+                // Some systems also look for these
+                resultIntent.putStringArrayListExtra("availableVoices", availableVoices)
+                
                 setResult(TextToSpeech.Engine.CHECK_VOICE_DATA_PASS, resultIntent)
             }
             "android.speech.tts.engine.GET_SAMPLE_TEXT" -> {
+                val lang = intent.getStringExtra("language")
+                Log.d(TAG, "GET_SAMPLE_TEXT for language: $lang")
+                
                 val sampleText = "این یک آزمایش از موتور بازگو کننده آوا است."
-                resultIntent.putExtra("sampleText", sampleText)
                 resultIntent.putExtra(TextToSpeech.Engine.EXTRA_SAMPLE_TEXT, sampleText)
                 setResult(RESULT_OK, resultIntent)
             }
             TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA -> {
+                // Since it's offline and included, we just say okay
                 setResult(RESULT_OK)
             }
             else -> setResult(RESULT_CANCELED)
